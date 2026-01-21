908c7db5e2413821f80fb017da17504032da2caf
LungstarCode <lungilemawila5@gmail.com>
2026-01-21 12:27:49 +0200

    successfully implemented the IntEqualityPrinter class

--- mastering_java_codeblocks_expressions_and_method_overloading/IntEqualityPrinter.java
+++ mastering_java_codeblocks_expressions_and_method_overloading/IntEqualityPrinter.java
@@ -1,3 +1,11 @@
 class IntEqualityPrinter {
     // write your code here!
+   
+    public static void printEqual(int x, int y, int z){
+        // condition that checks if the parameters are corect and displays a message!
+        if (x < 0 || y < 0 || z < 0) System.out.println("Invalid Value");
+        else if (x == y && y == z) System.out.println("All numbers are equal");
+        else if (x != y && y != z && x != z) System.out.println("All numbers are different");
+        else System.out.println("Neither all are equal or different");
+    }
 }
