54ac30e7d6b8c8801892a3ed63831a6e10af9081
LungstarCode <lungilemawila5@gmail.com>
2026-01-22 14:51:20 +0200

    added a new file in the exercises folder with the isLeap year class

---
+++ Coding Exercises/NumberOfDaysInMonth.java
@@ -0,0 +1,35 @@
+public class NumberOfDaysInMonth {
+    // write code here
+    public static boolean isLeapYear(int year){
+        if (year >= 1 && year <= 9999){
+            
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) return true;
+            else return false;
+            
+        } else return false;
+        
+    }
+    
+    public static int getDaysInMonth(int month, int year){
+        
+        if (month < 1 || month > 12 || year < 1 || year > 9999) return -1;
+        
+        switch (month){
+            case 1: 
+                    return 31;
+                    
+            case 3, 5, 7, 8,10, 12: 
+                    return 31;
+                   
+            case 4, 6, 9, 11:
+                    return 30;
+                   
+            case 2:
+                    if (isLeapYear(year)) return 29;
+                    else return 28;
+        };
+        
+           return -1;
+        
+    }
+}
