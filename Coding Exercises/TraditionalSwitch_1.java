a6055cce07265f5bf5ed6f7786dd16f579e7fe00
LungstarCode <lungilemawila5@gmail.com>
2026-01-22 11:19:20 +0200

    Added a new challeneg on basic or traditional switch statements or cases, solving the character to NATO's words

---
+++ Coding Exercises/TraditionalSwitch_1.java
@@ -0,0 +1,37 @@
+/*
+
+
+
+*/
+class TraditionalSwitch_1 {
+    public static void main(String[] args){
+        char alphabet = 'A';
+        char[] letters = {'A', 'B', 'C', 'D', 'E'};
+
+        for (char letter : letters){
+            alphabet = letter;
+            switch (alphabet) {
+                case 'A': 
+                        System.out.println("The letter is "+ alphabet + " and the NATO word is Able");
+                        break;
+                case 'B':
+                        System.out.println("The letter is "+ alphabet + " and the NATO word is Baker");
+                        break;
+                case 'C':
+                        System.out.println("The letter is "+ alphabet + " and the NATO word is Charlie");
+                        break;
+                case 'D':
+                        System.out.println("The letter is "+ alphabet + " and the NATO word is Dog");
+                        break;
+                case 'E':
+                        System.out.println("The letter is "+ alphabet + " and the NATO word is Easy");;
+                        break;
+                default: 
+                        System.out.println("The NATO word for "+ alphabet + " is not found!");
+                        break;
+                
+            }
+
+        }
+    }
+}
