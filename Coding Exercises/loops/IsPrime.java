9fd7da62f4d7d6f0dfd639c77d3227c1a2fbf4bd
LungstarCode <lungilemawila5@gmail.com>
2026-01-22 21:07:01 +0200

    updated the isPrime class to as a loop an d conditions that prints the prime numbers and count how many prime numbers were found and break once it gets 3 prime numbers

--- Coding Exercises/loops/IsPrime.java
+++ Coding Exercises/loops/IsPrime.java
@@ -1,6 +1,16 @@
 public class isPrime{
 
     public static void main(String[] args){
+        int primeCounter = 0;
+        for (int _i = 0; _i <= 1000 ; _i++){
+            if (isPrime(_i)){
+                primeCounter += 1;
+                System.out.println(_i + " is a prime number");
+            } 
+            if (primeCounter >= 3) break;
+        }
+
+        System.out.println("We found "+ primeCounter + " prime numbers");
 
     }
 
@@ -10,8 +20,6 @@ public class isPrime{
         for (int divisor = 2; divisor <= wholeNumber /2; divisor++){
             if (wholeNumber % 2 == 0) return false;
         }
-
-
         return true;
     }
 }
