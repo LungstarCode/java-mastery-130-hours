73641f7c359d4cb767ab8acc44975e0659a68877
LungstarCode <lungilemawila5@gmail.com>
2026-01-23 00:12:20 +0200

    added a while loop to the SumDigits class

--- Coding Exercises/loops/whileloops/SumDigits.java
+++ Coding Exercises/loops/whileloops/SumDigits.java
@@ -5,6 +5,13 @@ class SumDigits {
     }
     public static int sumDigits(int number){
         if (number < 0 ) return -1;
-        
+        if (number < 10) return number;
+        int sum = 0;
+
+        while (number >= 10){
+            if (number >= 10){
+
+            }
+        }
     }
 }
