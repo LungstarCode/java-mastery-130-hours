66c9d6957d32155b854f86d07c2bec04c5d0764d
LungstarCode <lungilemawila5@gmail.com>
2026-01-23 00:19:35 +0200

    edited the class SumDigits to complete the while loops, the program calculates the sum of each digit found on the number passed as an arguement to the sumDigits() method

--- Coding Exercises/loops/whileloops/SumDigits.java
+++ Coding Exercises/loops/whileloops/SumDigits.java
@@ -1,6 +1,8 @@
 class SumDigits {
 
     public static void main(String [] args){
+        System.out.println("The sum of all the digits in 1970 is "+ sumDigits(1970));
+        System.out.println("The sum of all the digits in 6 is "+ sumDigits(6));
 
     }
     public static int sumDigits(int number){
@@ -9,9 +11,11 @@ class SumDigits {
         int sum = 0;
 
         while (number >= 10){
-            if (number >= 10){
-
-            }
+            
+            sum += (number % 10);
+            number = number / 10;
         }
+        sum += number;
+        return sum;
     }
 }
