f4888c9cd06a880d2856aea7e786dd60b83d88fc
LungstarCode <lungilemawila5@gmail.com>
2026-01-22 20:53:08 +0200

    Added a new class in the /loop folder, this class checks if a given numnber is a prime number or not using a isPrime method

---
+++ Coding Exercises/loops/IsPrime.java
@@ -0,0 +1,17 @@
+public class isPrime{
+
+    public static void main(String[] args){
+
+    }
+
+    public static boolean isPrime(int wholeNumber){
+        if (wholeNumber <= 2) return (wholeNumber == 2);
+
+        for (int divisor = 2; divisor <= wholeNumber /2; divisor++){
+            if (wholeNumber % 2 == 0) return false;
+        }
+
+
+        return true;
+    }
+}
