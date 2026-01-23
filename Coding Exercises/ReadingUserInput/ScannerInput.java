39b62e642afe78d2b5df67138de9164102e89679
LungstarCode <lungilemawila5@gmail.com>
2026-01-23 14:11:40 +0200

    update: added variables in the ScannerInput class

--- Coding Exercises/ReadingUserInput/ScannerInput.java
+++ Coding Exercises/ReadingUserInput/ScannerInput.java
@@ -2,10 +2,17 @@ import java.util.Scanner;
 class Scanner {
 
     public static void getUserAge(int currentYear, String name){
+        // creating a scanner object using the System.in arg to receive user input
+        Scanner fetch = new Scanner(System.in);
+        int currentYear = 2026;
+
         System.out.println("What is your name: ");
         String name = fetch.nextLine();
 
         System.out.println("In what year were you born: ");
         String yearOfBirth = fetch.nextLine();
+        int year = Integer.parseInt(yearOfBirth);
+
+        
     }
 }
