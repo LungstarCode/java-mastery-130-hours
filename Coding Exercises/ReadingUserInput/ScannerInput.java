5afb053df6b4df88f3ad348beb0d0a4063e64a1d
LungstarCode <lungilemawila5@gmail.com>
2026-01-23 14:07:42 +0200

    added a new class that demonstrates the Scanner class

---
+++ Coding Exercises/ReadingUserInput/ScannerInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Scanner {
+
+    public static void getUserAge(int currentYear, String name){
+        System.out.println("What is your name: ");
+        String name = fetch.nextLine();
+
+        System.out.println("In what year were you born: ");
+        String yearOfBirth = fetch.nextLine();
+    }
+}
