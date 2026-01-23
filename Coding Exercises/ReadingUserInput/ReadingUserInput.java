e0c3dc5e82876c03fb56f32321a833875013977a
LungstarCode <lungilemawila5@gmail.com>
2026-01-23 12:40:57 +0200

    updated the class in ReadingUserInput to add teh console feature

--- Coding Exercises/ReadingUserInput/ReadingUserInput.java
+++ Coding Exercises/ReadingUserInput/ReadingUserInput.java
@@ -1,5 +1,18 @@
+import java.scanner; 
+
 public class ReadingUserData {
     public static void main(String[] args){
+        int currentYear = 2026;
         
+
+    }
+    public static String getInputFromConsole(int currentYear){
+        String name = Syste.console().readLine("Hi, what is your name: ");
+        System.out.println("Hello " + name + " thank you for learning java");
+        return "";
+    }
+
+    public static String getInputFromScanner(int currentYear){
+        return "";
     }
 }
