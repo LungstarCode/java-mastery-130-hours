58bd6d4f9bd7de92a2b3f890612723c341adde59
LungstarCode <lungilemawila5@gmail.com>
2026-01-24 23:39:49 +0200

    added a short description of the program at the top of the class body

--- Coding Exercises/ReadingUserInput/InputChallenge.java
+++ Coding Exercises/ReadingUserInput/InputChallenge.java
@@ -1,1 +1,9 @@
 // answering the user input challenge 
+public class InputChallenge {
+
+    """
+        read 5 valid numbers from the console and print the sum of all 5 numbers
+        tell the user incase of invalid numbers - "Invalid number" through the console
+        Prompt the user to enter number #X, x is number 1 through to 5
+    """
+}
