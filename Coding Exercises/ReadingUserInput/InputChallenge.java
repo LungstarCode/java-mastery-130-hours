c2565adf25faa70f1983f3d2f6e89549aa4aa6ac
LungstarCode <lungilemawila5@gmail.com>
2026-01-25 23:24:23 +0200

    fixed the description comment at the top class level

--- Coding Exercises/ReadingUserInput/InputChallenge.java
+++ Coding Exercises/ReadingUserInput/InputChallenge.java
@@ -2,11 +2,11 @@
 import java.util.Scanner;
 public class InputChallenge {
 
-    """
+    /*
         read 5 valid numbers from the console and print the sum of all 5 numbers
         tell the user incase of invalid numbers - "Invalid number" through the console
         Prompt the user to enter number #X, x is number 1 through to 5
-    """
+    */
 
     public static void main(String[] args ){
         addNumbers();
