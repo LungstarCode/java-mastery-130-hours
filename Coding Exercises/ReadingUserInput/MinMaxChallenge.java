0b329a736dcc51ecc858e7356e8cbd62b63cc7d1
LungstarCode <lungilemawila5@gmail.com>
2026-01-26 11:36:53 +0200

    Added a dummy logic of searching for minimum and maximum values and fixed the class declaration/defination

--- Coding Exercises/ReadingUserInput/MinMaxChallenge.java
+++ Coding Exercises/ReadingUserInput/MinMaxChallenge.java
@@ -1,11 +1,37 @@
 import java.util.Scanner;
-public class{
+public class MinMaxChallenge {
 
     public static void main(String[] args){
-
+        calculateMinMax();
     }
 
-    public void calculateMinMax(int number){
+    public static String calculateMinMax(){
         Scanner fetch = new Scanner(System.in);
+        int minNumber = 0;
+        int maxNumber = 0;
+        int uNumber = 0;
+        
+        while (true){
+            
+            System.out.println("Enter a number or any character to quit: ");
+            String uInput = fetch.nextLine();
+
+            try {
+                uNumber = Integer.parseInt(uInput);
+                if (uNumber < minNumber || uNumber < maxNumber) {
+                    minNumber = uNumber;
+                }
+                else if (uNumber > maxNumber){
+                    maxNumber = uNumber;
+                    }
+                }
+             catch (Exception invalidInput){
+                System.out.println("Exiting the loop: !!!!!");
+                System.out.println("The min is "+ minNumber + " and the max is " + maxNumber);
+                break;
+            };
+
+        }
+        return "The min is "+ minNumber + " and the max is " + maxNumber;
     }
 } 
