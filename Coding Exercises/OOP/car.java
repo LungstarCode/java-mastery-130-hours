62bb4ff8f260c188b87803c994b69d8b0ea55d38
LungstarCode <lungilemawila5@gmail.com>
2026-01-26 12:41:21 +0200

    Added code for both the Car and Main class

--- Coding Exercises/OOP/car.java
+++ Coding Exercises/OOP/car.java
@@ -1,4 +1,27 @@
 public class Car{
 
-    
+    private String make ="Omoda";
+    private String model = "C5 Elegence X";
+    private int year = 2025;
+    private String color = "Silver Black 2 toned";
+
+
+    public String getMake(){
+        return make;
+    }
+    public String getModel(){
+        return model;
+    }
+    public int getYear(){
+        return year;
+    }
+    public String getColor(){
+        return color;
+    }
+
+    public void printCarInfo(){
+        System.out.println("The car is a "+ color + " "+ year + " "+ make + " "+ model )
+    }
+
+
 }
