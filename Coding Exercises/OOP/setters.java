a16713156bbe42edb892b74cbbaa0b5d2a1a9bd8
LungstarCode <lungilemawila5@gmail.com>
2026-01-27 21:29:54 +0200

    Added a new class Setters that demonstarates the understanding of setters

---
+++ Coding Exercises/OOP/setters.java
@@ -0,0 +1,10 @@
+public class Setters {
+
+    private static String name;
+    private static int age;
+    private static String location;
+    private static String program;
+
+
+    // setter for each field
+}
