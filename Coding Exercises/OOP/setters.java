8a72869ced1f5963fa84a84f5ab0342b284d8404
LungstarCode <lungilemawila5@gmail.com>
2026-01-27 21:35:09 +0200

    Added the setters for all other data fields

--- Coding Exercises/OOP/setters.java
+++ Coding Exercises/OOP/setters.java
@@ -10,4 +10,16 @@ public class Setters {
     public setName(String name ){
         this.name = name; // Assigning the passed parameter to the name field
     }
+
+    public setAge(int age){
+        this.age = age;
+    }
+
+    public setProgram(String program){
+        this.program = program;
+    }
+
+    public setLocation(String location){
+        this.location = location;
+    }
 }
