62e923452a2f40ee6d1cae40cefb6dd4941565ee
LungstarCode <lungilemawila5@gmail.com>
2026-01-28 21:13:26 +0200

    added a new class Customer with 3 fields and their corresponding getters

---
+++ Coding Exercises/OOP/Customer.java
@@ -0,0 +1,13 @@
+class Customer{
+
+    private String name;
+    private double creditLimit;
+    public String emailAddress;
+
+    // getters 
+
+    public void getName() {return name;}
+    public void getCreditLimit(){return creditLimit;}
+    public void getEmailAddress(){return emailAddress;}
+
+}
