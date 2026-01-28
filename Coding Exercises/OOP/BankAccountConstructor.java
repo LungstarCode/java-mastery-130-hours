20b59f8581b56ea139c51750ce0e5bbd4070fc0f
LungstarCode <lungilemawila5@gmail.com>
2026-01-28 20:51:55 +0200

    added a clear logic and fixed errors, called the deopsit method in the main method

--- Coding Exercises/OOP/BankAccountConstructor.java
+++ Coding Exercises/OOP/BankAccountConstructor.java
@@ -14,29 +14,30 @@ public class BankAccount{
         this.balance = balance;
     }
     public static void main(String[] args ){
-
+        BankAccount lungilesBank = new BankAccount("Lungile", "12343543","0712107897", "lungilemawila5@gmail.com", 1000.00d);
+        lungilesBank.deposit(10000);
     }
 
     public static void deposit(double depositAmount){
         if (depositAmount > 0){
-            balance += depositAmoint;
-            System.out.println("A successful deposit of R"+ depositAmount + "
-             was made and your new balnce is R"+ balance);
-        } else System.out.print("You cannot deposit a negative amount!");
+            balance += depositAmount;
+            System.out.println("A successful deposit of R"+ depositAmount + 
+            " was made and your new balnce is R"+ balance);
+        } else {System.out.print("You cannot deposit a negative amount!");}
     }
 
     public static void withdraw(double withdrawalAmount){
         if (balance - withdrawalAmount > 0){
             balance -= withdrawalAmount;
-            System.out.println("Withdrawal of R"+ withdrawalAmount + "was 
-            successful, your new balance is R"+ balance);
-        } else System.out.println("Insufficent funds, R"+balance)
+            System.out.println("Withdrawal of R"+ withdrawalAmount + " was" + 
+            " successful, your new balance is R"+ balance);
+        } else {System.out.println("Insufficent funds, R"+balance);}
     }
 
     public String getName(){ return name;}
     public String getEmail() {return email;}
     public String getAccountNumber() {return accountNumber;}
     public String getCellNumber() {return cellNumber;}
-    public double getBalance() {return balance};
+    public double getBalance() {return balance;};
 
 }
