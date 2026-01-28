03e4b16ec6151c3a4e2a7d404707f3b423e90da7
LungstarCode <lungilemawila5@gmail.com>
2026-01-28 21:19:08 +0200

    added 3 constructors that initiated the fileds in different ways and uses constructor chaining

--- Coding Exercises/OOP/Customer.java
+++ Coding Exercises/OOP/Customer.java
@@ -4,6 +4,21 @@ class Customer{
     private double creditLimit;
     public String emailAddress;
 
+    // constructors 
+    public Customer(String name, String emailAddress, double creditLimit){
+        this.name = name;
+        this.creditLimit = creditLimit;
+        this.emailAddress = emailAddress;
+    }
+
+    public Customer(){
+        this("Lungile", "lungilemawila5@gmail.com", "1500.45");
+    }
+
+    public Customer(String name, String emailAddress){
+        this(name, emailAddress);
+    }
+
     // getters 
 
     public void getName() {return name;}
