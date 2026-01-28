aee0c55acb8be81dd3289abac0616fb6c3e6b088
LungstarCode <lungilemawila5@gmail.com>
2026-01-28 20:41:25 +0200

    added logic to the withdraw and deposit methods

--- Coding Exercises/OOP/BankAccountConstructor.java
+++ Coding Exercises/OOP/BankAccountConstructor.java
@@ -11,10 +11,20 @@ public class BankAccount{
 
     }
 
-    public static void deposit(double depositAmoint){
-        if (depositAmoint > 0){
+    public static void deposit(double depositAmount){
+        if (depositAmount > 0){
             balance += depositAmoint;
+            System.out.println("A successful deposit of R"+ depositAmount + "
+             was made and your new balnce is R"+ balance);
         } else System.out.print("You cannot deposit a negative amount!");
+    }
+
+    public static void withdraw(double withdrawalAmount){
+        if (balance - withdrawalAmount > 0){
+            balance -= withdrawalAmount;
+            System.out.println("Withdrawal of R"+ withdrawalAmount + "was 
+            successful, your new balance is R"+ balance);
+        } else System.out.println("Insufficent funds, R"+balance)
     }
 
     public String getName(){ return name;}
