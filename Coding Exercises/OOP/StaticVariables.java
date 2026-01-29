9ea69ad77854b88580b78234fa770a1eaead3f42
LungstarCode <lungilemawila5@gmail.com>
2026-01-29 10:46:27 +0200

    addded 2 instances of the class and initialized the name field

--- Coding Exercises/OOP/StaticVariables.java
+++ Coding Exercises/OOP/StaticVariables.java
@@ -1,5 +1,13 @@
 class StaticVar {
+    private static String name;
     public static void main(String[] args){
-        
+        StaticVar name1 = new StaticVar();
+        StaticVar name2 = new StaticVar();
+
+        name1.name = "Tshepo Serumule";
+        name2.name = "Lungile Mawila";
+
+        System.out.println(name1.name);
+        System.out.println(name2.name);
     }
 }
