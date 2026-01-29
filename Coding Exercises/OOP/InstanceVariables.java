12e80fd98f8e169db4e4ed7bf940f4326378720d
LungstarCode <lungilemawila5@gmail.com>
2026-01-29 10:23:45 +0200

    added two objects of the class initializing two instances of tyhe name filed

--- Coding Exercises/OOP/InstanceVariables.java
+++ Coding Exercises/OOP/InstanceVariables.java
@@ -1,7 +1,16 @@
 public class InstanceVar {
-
+    private String name;
 
     public static void main(String[] args){
+        InstanceVar obj = new InstanceVar();
+        obj.name = "Lungile Mawila";
+        InstanceVar obj1 = new InstanceVar();
+        obj1.name = "Tshepo Serumule";
+
+        System.out.println(obj.name);
+        System.out.println(obj1.name);
         
     }
+
+
 }
